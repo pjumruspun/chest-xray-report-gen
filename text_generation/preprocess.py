Add dataset (unfinished), fix dones bug, gradient accumulation

text_generation/preprocess.py
@@ -1,9 +1,8 @@
 import pickle
 import os
-
+import time
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from skimage.io import imread
 from skimage.transform import resize
 from tensorflow.keras.applications.densenet import preprocess_input
@@ -88,27 +87,98 @@ def load_image_mappings() -> dict:
         image_mappings = pickle.load(f)
         return image_mappings
 
-
-def load_dataframe() -> pd.DataFrame:
+def read_report(path):
     """
-    Returns a joined dataframe consists of uid, filename, projection, and report.
-    Using indiana_projections.csv and indiana_reports.csv
+    For mimic-cxr dataset
     """
 
-    COLUMNS_TO_RETURN = ['uid', 'img_path', 'Problems', 'projection', 'report']
+    with open(path, 'r') as f:
+        report = f.read()
+        start = report.find('FINDINGS:')
+        end = report.find('IMPRESSION:')
+        report = report[start+9:end]
+        report = report.replace('\n', ' ')
+        report = ' '.join(report.split())
 
-    # Read the csv files
-    report_df = pd.read_csv(configs['report_csv'])
-    proj_df = pd.read_csv(configs['projection_csv'])
+    return report
 
-    df = pd.merge(proj_df, report_df, left_on='uid', right_on='uid')
-    df['report'] = df['findings'].fillna('') + df['impression'].fillna('')
+def load_dataframe(dataset) -> pd.DataFrame:
+    """
+    if dataset == 'iu-xray':
+        Returns a joined dataframe consists of uid, filename, projection, and report.
+        Using indiana_projections.csv and indiana_reports.csv
+    elif dataset == 'mimic-cxr':
 
-    # Full path
-    df['img_path'] = configs['image_path'] + df['filename']
+    """
 
-    return df[COLUMNS_TO_RETURN]
+    if dataset == 'iu-xray':
 
+        COLUMNS_TO_RETURN = ['uid', 'img_path', 'Problems', 'projection', 'report']
+
+        # Read the csv files
+        report_df = pd.read_csv(configs['report_csv'])
+        proj_df = pd.read_csv(configs['projection_csv'])
+
+        df = pd.merge(proj_df, report_df, left_on='uid', right_on='uid')
+        df['report'] = df['findings'].fillna('') + df['impression'].fillna('')
+
+        # Full path
+        df['img_path'] = configs['image_path'] + df['filename']
+        df = df[COLUMNS_TO_RETURN]
+        return df
+
+    elif dataset == 'mimic-cxr':
+        MIMIC_DIR = configs['mimic_dir']
+        COLUMNS_TO_RETURN = ['dicom_id', 'subject_id', 'study_id', 
+                        'ViewPosition', 'img_path', 'report_path', 'No Finding']
+
+        record_df = pd.read_csv(MIMIC_DIR + 'cxr-record-list.csv')
+        study_df = pd.read_csv(MIMIC_DIR + 'cxr-study-list.csv')
+
+        merged = pd.merge(record_df, study_df, 
+                            left_on=['subject_id', 'study_id'], 
+                            right_on=['subject_id', 'study_id'])
+        
+        chexpert_df = pd.read_csv(MIMIC_DIR + 'mimic-cxr-2.0.0-chexpert.csv')[['subject_id', 'study_id', 'No Finding']]
+        merged = pd.merge(merged, chexpert_df, 
+                            left_on=['subject_id', 'study_id'], 
+                            right_on=['subject_id', 'study_id'])
+        
+        merged['No Finding'] = merged['No Finding'].fillna(0.0)
+
+        meta_df = pd.read_csv(MIMIC_DIR + 'mimic-cxr-2.0.0-metadata.csv')
+        merged = pd.merge(meta_df, merged, 
+                            left_on=['subject_id', 'study_id', 'dicom_id'], 
+                            right_on=['subject_id', 'study_id', 'dicom_id'])
+
+        # Rename columns
+        merged = merged.rename(columns={'path_x': 'img_path', 'path_y': 'report_path'})
+
+        # Filter columns
+        merged = merged[COLUMNS_TO_RETURN]
+
+        # Make full path
+        merged['img_path'] = merged['img_path'].str.replace('files/', MIMIC_DIR + 'images512/')
+        merged['img_path'] = merged['img_path'].str.replace('.dcm', '.png')
+        merged['report_path'] = merged['report_path'].str.replace('files/', MIMIC_DIR + 'reports/files/')
+
+        # Convert to report
+        start_time = time.time()
+        print(f"Total rows: {merged.shape[0]}")
+        print("Converting report_path to report...")
+        tqdm.pandas()
+        merged['report'] = merged['report_path'].progress_apply(lambda x: read_report(x))
+        print(f"Total time taken: {time.time() - start_time:.4f}s")
+
+        # Drop missing reports
+        merged = merged[merged['report'].str.len() > 0]
+        merged = merged.reset_index(drop=True)
+
+        # Drop rows start with FINAL
+        merged = merged[~merged['report'].str.startswith('FINAL')]
+        merged = merged.reset_index(drop=True)
+
+        return merged
 
 def load_images(img_paths, preprocess=True) -> np.array:
     """
@@ -155,7 +225,6 @@ def generate_image_features(images, batch_size=32) -> np.array:
 
     return np.asarray(image_features)
 
-
 def create_image_mappings(img_paths, image_features) -> dict:
 
     if len(img_paths) != image_features.shape[0]:
@@ -163,32 +232,24 @@ def create_image_mappings(img_paths, image_features) -> dict:
 
     return dict(zip(img_paths, image_features))
 
-def limitgpu(maxmem):
-	gpus = tf.config.list_physical_devices('GPU')
-	if gpus:
-		# Restrict TensorFlow to only allocate a fraction of GPU memory
-		try:
-			for gpu in gpus:
-				tf.config.experimental.set_virtual_device_configuration(gpu,
-						[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=maxmem)])
-		except RuntimeError as e:
-			# Virtual devices must be set before GPUs have been initialized
-			print(e)
-
 if __name__ == "__main__":
     # TODO: Add command line options to do either
     #       just image or report preprocessing
-    df = load_dataframe()
+    dataset = configs['dataset']
+    create_new = False
+    
+    if create_new:
+        df = load_dataframe(dataset)
 
-    # preprocess reports
-    df = preprocess_text(df)
-    print(f"Total report rows: {df.shape[0]}")
+        # preprocess reports
+        df = preprocess_text(df)
+        print(f"Total report rows: {df.shape[0]}")
 
-    # save to csv file
-    save_csv(df)
+        print(df.head())
+        print(df.columns)
+        print(f"Total rows: {df.shape[0]}")
+        print(f"No finding ratio: {df['No Finding'].mean()}")
+        df.to_csv(configs['mimic_csv_file_path'])
 
-    # preprocess images
-    image_mappings = preprocess_images(df)
-
-    # save to pickle file
-    save_image_mappings(image_mappings)
+    else:
+        df = pd.read_csv(configs['mimic_csv_file_path'])

text_generation/pytorch_dataset.py
@@ -1,5 +1,4 @@
 from torch.utils.data import Dataset
-from dataset import files_exist
 from configs import configs
 import pandas as pd
 from skimage.io import imread
@@ -20,33 +19,30 @@ class ChestXRayDataset(Dataset):
         data_split: train, val, or test
         augment_func: augments the image, leave None to do nothing
         """
-        if files_exist():
-            if data_split == 'train':
-                data_path = configs['train_csv']
-            elif data_split == 'val':
-                data_path = configs['val_csv']
-            elif data_split == 'test':
-                data_path = configs['test_csv']
-            else:
-                raise AttributeError(f"Invalid data_split: {data_split}")
-
-            # Load dataframe
-            df = pd.read_csv(data_path)
-
-            # Stores captions and image paths
-            self.captions = df['report'].values
-            self.caplens = df['report'].str.split().apply(lambda x: len(x)).values
-            self.image_paths = df['img_path'].values
-            self.data_split = data_split
-            self.input_shape = configs['input_shape']
-            self.augment_func = augment_func
-            self.dataset_size = self.captions.shape[0]
-            self.tokenizer = tokenizer
-            self.pad_length = get_max_report_len()
-            self.transform = transform
-            self.cpi = 1
+        if data_split == 'train':
+            data_path = configs['train_csv']
+        elif data_split == 'val':
+            data_path = configs['val_csv']
+        elif data_split == 'test':
+            data_path = configs['test_csv']
         else:
-            raise FileNotFoundError()
+            raise AttributeError(f"Invalid data_split: {data_split}")
+
+        # Load dataframe
+        df = pd.read_csv(data_path)
+
+        # Stores captions and image paths
+        self.captions = df['report'].values
+        self.caplens = df['report'].str.split().apply(lambda x: len(x)).values
+        self.image_paths = df['img_path'].values
+        self.data_split = data_split
+        self.input_shape = configs['input_shape']
+        self.augment_func = augment_func
+        self.dataset_size = self.captions.shape[0]
+        self.tokenizer = tokenizer
+        self.pad_length = get_max_report_len()
+        self.transform = transform
+        self.cpi = 1
 
     def __getitem__(self, index):
         """
@@ -123,3 +119,4 @@ if __name__ == "__main__":
     print(allcaps)
     plt.imshow(image.numpy())
     plt.show()
+    print(f"Image size: {image.numpy().shape}")

text_generation/pytorch_finetune.py
@@ -1,29 +1,31 @@
-import torch
+import argparse
+
+import matplotlib.pyplot as plt
 import numpy as np
-import torchvision.transforms as transforms
+import torch
 import torch.nn.functional as F
+import torchvision.transforms as transforms
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from pytorch_dataset import ChestXRayDataset
-from pytorch_chest_xray_env import ChestXRayEnv
-from pytorch_tokenizer import create_tokenizer
-from pytorch_label import temperature_sampling
-from pytorch_train import validate, save_checkpoint
-from utils import decode_sequences
 from tqdm import tqdm
-import matplotlib.pyplot as plt
+
+from pytorch_chest_xray_env import ChestXRayEnv
+from pytorch_dataset import ChestXRayDataset
+from pytorch_label import temperature_sampling
+from pytorch_tokenizer import create_tokenizer
+from pytorch_train import save_checkpoint, validate
+from utils import decode_sequences
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 max_len = 100
 temperature = 1.0
 eps = np.finfo(np.float32).eps.item()
 print_freq = 20
+accumulation_iter = 4
 tb = SummaryWriter()
-lr = 1e-3
-metrics = 'recall'
 
-def finetune(encoder, decoder, train_loader, tokenizer, envs, epoch):
+def finetune(encoder, decoder, train_loader, tokenizer, envs, epoch, lr):
     encoder.train()
     decoder.train()
     optimizer = torch.optim.Adam(decoder.parameters(), lr=lr)
@@ -44,89 +46,39 @@ def finetune(encoder, decoder, train_loader, tokenizer, envs, epoch):
         
         # Variables before running an opisode
         dones = [False] * batch_size
-        res = [[] for _ in range(batch_size)] 
         ep_rewards = [[] for _ in range(batch_size)]
         
         seqs, _ = temperature_sampling(
             encoder, decoder, tokenizer, images=imgs, 
             temperature=temperature, max_len=max_len, rl=True)
-        
-        # print([tokenizer.itos[e] for e in seqs[0]])
-        # print(len(seqs[0]))
-        # print(decoder.saved_actions)
 
         for j, (env, seq) in enumerate(zip(envs, seqs)):
             for action in seq:
                 s, r, done, info = env.step(action)
-                if done: 
-                    break
-                    
-                # print(f"{tokenizer.itos[action]}, r: {r}")
+                dones[j] = done
                 ep_rewards[j].append(r)
+                if all(dones):
+                    break
 
         # Append results
         final_rewards = [sum(ep_reward) for ep_reward in ep_rewards]
-        loss, policy_loss, value_loss = finish_episode(decoder, ep_rewards, optimizer)
+        loss, policy_loss, value_loss = finish_episode(i, len(train_loader), decoder, ep_rewards, optimizer)
         reward_history.extend(final_rewards)
 
         if loss is not None:
             losses.append(loss)
             policy_losses.append(policy_loss)
             value_losses.append(value_loss)
-            ith = i + len(train_loader)
+            ith = i + len(train_loader) * epoch
             tb.add_scalar("Episode_reward", np.mean(final_rewards), ith)
             tb.add_scalar("Loss", loss, ith)
             tb.add_scalar("Policy_loss", policy_loss, ith)
             tb.add_scalar("Value_loss", value_loss, ith)
-            tb.add_text("Generated_sentence", decode_sequences(tokenizer, seqs)[0], i)
+            tb.add_text("Generated_sentence", decode_sequences(tokenizer, seqs)[0], ith)
 
         if ((i + 1) % print_freq) == 0:
             print(f"\nloss avg: {np.mean(losses):.4f}\tlast {print_freq} loss avg: {np.mean(losses[-print_freq:])}")
 
-
-# def finish_episode(decoder, ep_rewards, optimizer):
-#     gamma = 0.99
-#     policy_loss = []
-#     value_loss = []
-
-#     # Batch size 1 for now
-#     model_rewards = ep_rewards[0]
-#     print(f"Model rewards: {model_rewards}")
-#     # return(t) = return(t-1) + gamma * 
-#     returns = get_expected_returns(model_rewards, gamma, normalize=True)
-#     print(f"Returns: {[round(g.item(), 2) for g in returns]}")
-
-#     for (log_prob, value), g in zip(decoder.saved_actions, returns):
-#         # Minimize -log(pi) * (return - v)
-#         # -log_prob always positive, but delta sometimes negative and make total policy loss negative
-#         # How to fix this????
-#         # delta = torch.abs(g - value.item())
-#         delta = g - value.item()
-#         policy_loss.append(-torch.squeeze(log_prob) * delta)
-#         print(f"-log_prob:{round(-torch.squeeze(log_prob).item(), 2)}\tg: {g}\tvalue: {value.item()}\tdelta: {round(delta.item(), 2)}")
-
-#         # Try minimize -log(pi) * v?
-#         # policy_loss.append(-torch.squeeze(log_prob) * value)
-#         # print(f"-log_prob:{round(-torch.squeeze(log_prob).item(), 2)}\tg: {g}\tvalue: {value.item()}")
-
-#         # Huber loss, less fluctuative than squared loss
-#         v_loss = F.smooth_l1_loss(value, torch.tensor([g]).to(device))
-#         value_loss.append(v_loss)
-
-#     optimizer.zero_grad()
-#     # print(f"{policy_loss=}, {value_loss=}")
-#     if len(policy_loss) != 0 and len(value_loss) != 0:
-#         total_policy_loss = torch.stack(policy_loss).mean()
-#         total_value_loss = torch.stack(value_loss).mean()
-#         loss = total_policy_loss + total_value_loss
-#         loss.backward()
-#         optimizer.step()
-#         # print(f"policy_loss={[round(pl.item(), 2) for pl in policy_loss]}\nvalue_loss={[round(vl.item(), 2) for vl in value_loss]}\n{round(loss.item(), 2)=}")
-
-#         decoder.saved_actions = []
-#         return loss.item(), total_policy_loss.item(), total_value_loss.item()
-#     else:
-#         return None
 
 def ragged_list_to_tensor(nested_ls, default_value):
     # only works on 2D nested list
@@ -141,7 +93,8 @@ def ragged_list_to_tensor(nested_ls, default_value):
 
     return res, lengths
 
-def finish_episode(decoder, ep_rewards, optimizer):
+
+def finish_episode(i, loader_length, decoder, ep_rewards, optimizer):
     gamma = 0.99
     total_policy_losses = torch.tensor([0.0], device=device, requires_grad=True)
     total_value_losses = torch.tensor([0.0], device=device, requires_grad=True)
@@ -150,8 +103,6 @@ def finish_episode(decoder, ep_rewards, optimizer):
     # Convert to padded tensors
     model_rewards, r_lengths = ragged_list_to_tensor(ep_rewards, default_value=float('nan'))
     saved_actions, s_lengths = ragged_list_to_tensor(decoder.saved_actions, default_value=torch.tensor([float('nan'), float('nan')], device=device, requires_grad=True))
-    # print(f"Model rewards: {model_rewards}")
-    # print(f"Lengths: {lengths}")
 
     model_rewards.to(device)
     saved_actions.to(device)
@@ -162,18 +113,9 @@ def finish_episode(decoder, ep_rewards, optimizer):
     model_rewards = model_rewards[r_sort_ind] # (batch_size, max_seq_len)
     saved_actions = saved_actions[s_sort_ind] # (batch_size, max_seq_len)
 
-    # print(f"Lengths after sorting: {lengths}")
-    # print(f"{r_lengths}\t{s_lengths}")
-    # print(f"{r_sort_ind=}\t{s_sort_ind}")
-    # print(f"Model rewards after sorting: {model_rewards}")
-
     # Transpose so we can loop through each token
-    # print(model_rewards.shape)
-    # print(saved_actions.shape)
     model_rewards = torch.t(model_rewards) # (max_seq_len, batch_size)
     saved_actions = torch.transpose(saved_actions, 0, 1) # (max_seq_len, batch_size)
-    # print(model_rewards.shape)
-    # print(saved_actions.shape)
 
     # Accumulated reward batched
     accu_rewards = torch.zeros(len(ep_rewards), device=device)
@@ -193,40 +135,33 @@ def finish_episode(decoder, ep_rewards, optimizer):
         adv = rewards + gamma * values_next - values
 
         # Calculate policy losses
-        policy_loss = log_probs * adv
+        policy_loss = -log_probs * adv
 
         # Calculate value losses
         # Huber loss, less fluctuative than squared loss
         value_loss = F.smooth_l1_loss(values, accu_rewards[:batch_size])
-        
-
-        # print(f"{i=}\t{batch_size=}\n{log_probs=}\n{values=}\n{values_next=}\n{rewards=}\n{adv=}")
-        # print(f"{value_loss=}")
 
         # L = avg(log_prob * adv)
         total_policy_losses = total_policy_losses + torch.sum(policy_loss)
         total_value_losses = total_value_losses + torch.sum(value_loss)
-        # print(policy_loss)
-        # print(total_policy_losses)
-        # print(value_loss)
-        # print(total_value_losses)
-
+        
+        # Total batch size as divisor
         total_batch_size += batch_size
         
-        
-    
-    optimizer.zero_grad()
-    # policy_losses = torch.Tensor(policy_losses)
-    # value_losses = torch.Tensor(value_losses)
-
-    # print(f"{policy_loss=}, {value_loss=}")
     total_policy_losses = total_policy_losses / total_batch_size
     total_value_losses = total_value_losses / total_batch_size
     loss = total_policy_losses + total_value_losses
     loss.backward()
-    optimizer.step()
+
+    # Gradient accumulation
+    if (i + 1) % accumulation_iter == 0 or (i + 1) == loader_length:
+        optimizer.step()
+        optimizer.zero_grad()
+
     # print(f"policy_loss={[round(pl.item(), 2) for pl in total_policy_losses]}\nvalue_loss={[round(vl.item(), 2) for vl in total_value_losses]}\n{round(loss.item(), 2)=}")
+    
     return loss.item(), total_policy_losses.item(), total_value_losses.item()
+
 
 def get_expected_returns(rewards, gamma, normalize=True):
     returns = []
@@ -243,15 +178,23 @@ def get_expected_returns(rewards, gamma, normalize=True):
 
 
 def main():
-    train_batch_size = 12
-    val_batch_size = 12
-    epochs = 5
+    parser = argparse.ArgumentParser(description='Finetune existing model with actor critic')
+    parser.add_argument('--learning-rate', '-lr', type=float, help='Learning rate')
+    parser.add_argument('--epoch', '-e', type=int, help='Learning rate')
+    parser.add_argument('--metrics', '-m', type=str, help='Metrics for reward module', choices=['f1', 'recall', 'precision'])
+    parser.add_argument('--batch_size', '-bs', type=int, help='Batch size for both training and validating')
+    parser.add_argument('--checkpoint', '-c', type=str, help='Relative checkpoint path, should be pth.tar file')
+    args = parser.parse_args()
+
+    train_batch_size = args.batch_size
+    val_batch_size = args.batch_size
+    epochs = args.epoch
     tokenizer = create_tokenizer()
     sparse_reward = False
     num_instances = train_batch_size
 
     # Models
-    checkpoint_path = "weights\pytorch_attention\checkpoint_2022-02-08_08-33-18.193576.pth.tar"
+    checkpoint_path = args.checkpoint
     checkpoint = torch.load(checkpoint_path)
     encoder = checkpoint['encoder']
     decoder = checkpoint['decoder']
@@ -283,13 +226,13 @@ def main():
     # Create env (Parallel)
     envs = []
     for _ in range(num_instances):
-        env = ChestXRayEnv(tokenizer, max_len, sparse_reward=sparse_reward, metrics=metrics)
+        env = ChestXRayEnv(tokenizer, max_len, sparse_reward=sparse_reward, metrics=args.metrics)
         env.reset()
         envs.append(env)
 
     for epoch in range(epochs):
         print(f"Finetuning epoch {epoch+1}")
-        finetune(encoder, decoder, train_loader, tokenizer, envs, epoch)
+        finetune(encoder, decoder, train_loader, tokenizer, envs, epoch, args.learning_rate)
         recent_bleu4 = validate(val_loader, encoder, decoder, loss_function, tokenizer)
         print(f"Bleu4: {recent_bleu4}")
         save_checkpoint(epoch, encoder, decoder, encoder_optimizer, decoder_optimizer, recent_bleu4)

text_generation/pytorch_test.py
@@ -287,7 +287,7 @@ def main():
         # "weights\pytorch_attention\checkpoint_2021-12-14_14-23-42.430392.pth.tar",
         # 'weights/pytorch_attention/checkpoint_2022-01-07_17-44-48.868114.pth.tar', # <- RL
         # 'weights/pytorch_attention/checkpoint_2022-01-18_19-05-33.048100.pth.tar', # <- working RL
-        # 'weights\pytorch_attention\checkpoint_2022-02-08_01-29-52.625176.pth.tar', # <- cleaned epoch 20 0.2957 macro f1
+        'weights\pytorch_attention\checkpoint_2022-02-08_01-29-52.625176.pth.tar', # <- cleaned epoch 20 0.2957 macro f1
         'weights\pytorch_attention\checkpoint_2022-02-08_10-37-10.316094.pth.tar', # <- cleaned RL epoch 3
     ]
 
@@ -309,7 +309,7 @@ def main():
 
     for checkpoint_path in checkpoint_paths:
         for temperature in temperatures:
-            for attempt in range(1):
+            for attempt in range(5):
                 print("Preparing models...")
                 
                 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -338,7 +338,7 @@ def main():
                         gts = decode_sequences(tokenizer, caps)
                         results['ground_truth'].extend(gts)
 
-                unique_name = f"RL_temp_{temperature}"
+                unique_name = f"{checkpoint_path}_{attempt+1}"
 
                 df = pd.DataFrame(results)
                 df.to_csv(unique_name + '_' + configs['prediction_file_name'])